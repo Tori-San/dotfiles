66eb41f561717b5b8a97294a69534663c063e6d0
Kai Vogelgesang <s8kavoge@stud.uni-saarland.de>
2018-02-06 12:52:15 +0100

    Update themer activator to reload powerline daemon

--- scripts/tmux-powerline.py
+++ scripts/tmux-powerline.py
@@ -4,4 +4,5 @@ import os
 class TmuxActivator(ThemeActivator):
     """Tell tmux to reload powerline"""
     def activate(self):
+        os.system('powerline-daemon -r')
         os.system('powerline-config tmux setup')

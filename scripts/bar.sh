a5829f4078b2e98583285be44fb71b0cf562d269
Kai Vogelgesang <s8kavoge@stud.uni-saarland.de>
2018-04-15 16:23:03 +0200

    Update bar launcher script

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -2,13 +2,13 @@
 killall powerline-lemonbar; \
 powerline-lemonbar\
     --i3 \
-    --alt_output \
     --bar_command lemonbar \
-    --height 13 \
+    --height 16 \
     --interval 2 \
     -- \
     -a 40 \
     -b \
-    -f '-xos4-terminesspowerline-medium-*-*-*-12-*-*-*-*-*-*-*' \
-    -f 'FontAwesome-8' \
-    -f 'IPAGothic-8'
+    -f 'Hack-11' \
+    -f 'PowerlineSymbols-12' \
+    -f 'FontAwesome-11' \
+    -f 'IPAGothic-11'

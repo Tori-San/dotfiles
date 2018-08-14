5ada64536ee7b2cca174e23bf2526da2402937bd
Kai Vogelgesang <s8kavoge@stud.uni-saarland.de>
2018-08-14 10:20:54 +0200

    Update aliases to use yay instead of yaourt

--- alias.zsh
+++ alias.zsh
@@ -32,8 +32,6 @@ alias clip="xclip -selection clipboard"
 
 alias pubip="curl -s ipv4.icanhazip.com"
 
-alias vserver="ssh -A dratini@v22017014164742712.powersrv.de"
-
 alias netrestart="systemctl restart netctl-auto@wlp5s0.service"
 alias btrestart="systemctl restart bluetooth"
 
@@ -50,3 +48,5 @@ alias vim="vim --servername vim"
 alias gds="git diff --staged"
 
 alias tagify="find -L . -regex \".*\.\(h\|c\|hpp\|cpp\)\" | xargs gcc -M | sed -e 's/[\\ ]/\n/g' | sed -e '/^$/d' -e '/\.o:[ \t]*$/d' | ctags -L - -I __attribute__,__attribute_deprecated__,__attribute_format_arg__,__attribute_format_strfmon__,__attribute_malloc__,__attribute_noinline__,__attribute_pure__,__attribute_used__,__attribute_warn_unused_result__,__wur,__THROW,__nonnull+ --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --c++-kinds=+p --fields=+liaS --extra=+q"
+
+alias yaourt="xdg-open http://leafbla.de/redirect.html"

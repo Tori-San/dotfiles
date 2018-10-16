800d4444b0bd685174003086d3f5f232084e7f93
Kai Vogelgesang <s8kavoge@stud.uni-saarland.de>
2018-10-16 12:09:28 +0200

    Update bar launcher script

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -9,6 +9,7 @@ powerline-lemonbar\
     -a 40 \
     -b \
     -f 'Hack-11' \
-    -f 'PowerlineSymbols-12' \
+    -f 'PowerlineSymbols-11' \
     -f 'FontAwesome-11' \
-    -f 'IPAGothic-11'
+    -f 'IPAGothic-11' \
+    -o 1 -o 0 -o 0 -o 0
